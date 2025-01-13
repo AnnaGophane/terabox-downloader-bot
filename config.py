7323e6ab54434cf0cff76af64c9e1e641187bef9
Ajstyle <92094333+AnnaGophane@users.noreply.github.com>
2025-01-13 17:55:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 22620071  # api id
+API_HASH = "85b7c68ac5da126389cfb9bbb89a5190"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7902732073:AAFVrEh5cyaJXhmur-vOCSMUIoay5DyLtXg"  # bot token
 
 
 # REDIS
-HOST = "localhost"  # redis host uri
+HOST = "redis-14201.c15.us-east-1-2.ec2.redns.redis-cloud.com:14201"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "1hlUwaWzkStWzk0AcdiSSUBIwkHBKARc"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002315965012  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
 COOKIE = ""
-ADMINS = [1317173146]
+ADMINS = [5321385511]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "Terabox_Links_To_Videos_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@Terabox_Links_To_Videos"
 
-PUBLIC_EARN_API = ""  # https://publicearn.com/api
+PUBLIC_EARN_API = "853a417e6360effc641fbe39633b730869df8e21"  # https://publicearn.com/api
